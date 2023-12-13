207fc4f7d2f5d2a045e9f0fcbc1741ba139fdc4e
AAV1ASOV <avworkemails@gmail.com>
2023-12-14 00:30:13 +0500

    module1-task3 final

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
 import SortPresenter from './presenter/sort-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
-import FormViewPresenter from './presenter/form-presenter.js';
 import PointsListPresenter from './presenter/points-list-presenter.js';
-import EditFormViewPresenter from './presenter/edit-form-presenter.js';
 
 const tripEventsNode = document.querySelector('.trip-events');
 const filterNode = document.querySelector('.trip-controls__filters');
 
 const sortPresenter = new SortPresenter({ sortContainer: tripEventsNode });
-const formPresenter = new FormViewPresenter({ formContainer: tripEventsNode });
 const filterPresenter = new FilterPresenter({ filterContainer: filterNode });
 const pointListPresenter = new PointsListPresenter({ pointsListContainer: tripEventsNode });
-const editFormViewPresenter = new EditFormViewPresenter({ editFormContainer: tripEventsNode });
 
 filterPresenter.init();
 sortPresenter.init();
-formPresenter.init();
 pointListPresenter.initPointList();
-for (let i = 0; i < 3; i++) {
-  pointListPresenter.initPoint();
-}
-editFormViewPresenter.init();
+
--- src/presenter/points-list-presenter.js
+++ src/presenter/points-list-presenter.js
@@ -1,28 +1,21 @@
 import { render } from '../render.js';
 import PointView from '../view/point-view.js';
 import PointListView from '../view/point-list-view.js';
+import EditFormView from '../view/edit-form-view.js';
 
 export default class PointsListPresenter {
-  pointsListComponent = new PointListView();
-
-  getPointComponent() {
-    return new PointView();
-  }
-
   constructor({ pointsListContainer }) {
     this.pointsListContainer = pointsListContainer;
+    this.pointsContainer = new PointListView();
   }
 
   initPointList() {
-    render(this.pointsListComponent, this.pointsListContainer);
-  }
+    render(this.pointsContainer, this.pointsListContainer);
+    render(new EditFormView(), this.pointsContainer.getElement());
 
-  initPoint() {
-    render(this.getPointComponent(), this.pointsListComponent.getElement());
-  }
-
-  getPointListElement() {
-    return this.pointsListComponent;
+    for (let i = 0; i < 3; i++) {
+      render(new PointView(), this.pointsContainer.getElement());
+    }
   }
 }
 
